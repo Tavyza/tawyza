7b0e67a8c889f21a86d5b191ca53056a247b55fe
Tshema <120181836+Tshema@users.noreply.github.com>
2023-07-18 14:08:57 -0400

    Update setup.lua

--- system_files/setup.lua
+++ system_files/setup.lua
@@ -1,7 +1,7 @@
 print("Installing system files... (This may take a bit of time)")
 
-wget https://raw.githubusercontent.com/Tshema/tawyza/main/system_files/main_system.lua
-wget https://raw.githubusercontent.com/Tshema/tawyza/main/system_files/command_prompt.lua
-wget https://raw.githubusercontent.com/Tshema/tawyza/main/system_files/file_manager.lua
-wget https://raw.githubusercontent.com/Tshema/tawyza/main/system_files/systemsettings.lua
+os.execute("wget https://raw.githubusercontent.com/Tshema/tawyza/main/system_files/main_system.lua")
+os.execute("wget https://raw.githubusercontent.com/Tshema/tawyza/main/system_files/command_prompt.lua")
+os.execute("wget https://raw.githubusercontent.com/Tshema/tawyza/main/system_files/file_manager.lua")
+os.execute("wget https://raw.githubusercontent.com/Tshema/tawyza/main/system_files/systemsettings.lua")
 
