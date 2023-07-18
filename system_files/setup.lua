7b8b24e5a6b8764215672c412b3255b0fe857de3
Tshema <120181836+Tshema@users.noreply.github.com>
2023-07-18 13:56:06 -0400

    a

--- system_files/setup.lua
+++ system_files/setup.lua
@@ -1,13 +1,7 @@
 print("Installing system files... (This may take a bit of time)")
 
-
-os.execute("wget https://github.com/Tshema/TawyzaOS/system_files/main_system.lua")
-os.execute("wget https://github.com/Tshema/TawyzaOS/system_files/command_prompt.lua")
-os.execute("wget https://github.com/Tshema/TawyzaOS/system_files/file_manager.lua")
-os.execute("wget https://github.com/Tshema/TawyzaOS/system_files/systemsettings.lua")
-os.execute("wget https://github.com/Tshema/TawyzaOS/system_files/autorun.lua")
-if success then
-    print("System files installed.")
-else
-    print("System files could not be installed."...reason)
+wget("https://github.com/Tshema/tawyza/blob/main/system_files/main_system.lua")
+wget("https://github.com/Tshema/tawyza/blob/main/system_files/command_prompt.lua")
+wget("https://github.com/Tshema/tawyza/blob/main/system_files/file_manager.lua")
+wget("https://github.com/Tshema/tawyza/blob/main/system_files/systemsettings.lua")
 end
