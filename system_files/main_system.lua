be430808dc57ed409904183e92656acc9ed2c144
Tshema <120181836+Tshema@users.noreply.github.com>
2023-07-18 20:55:55 -0400

    Update main_system.lua

--- system_files/main_system.lua
+++ system_files/main_system.lua
@@ -3,7 +3,7 @@ local event = require("event")
 local gpu = component.gpu
 local term = require("term")
 
-local w, h = gpu.getResolution()
+local w, h = gpu.gesolution()
 
 local options = {
     "File manager",
